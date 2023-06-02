085dc93edf99d70b3a5e3b3866bc0b0f74618970
Aiza Regalado <121596529+AizaRegalado@users.noreply.github.com>
2023-06-02 19:30:51 +0800

    Create Top destination page.php

---
+++ My Tourism Guide/Top destination page.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Tourism Guide</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">  
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">                
+    <link rel="stylesheet" href="css/bootstrap.min.css">                                      
+    <link rel="stylesheet" type="text/css" href="css/datepicker.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+    <link rel="stylesheet" href="css/templatemo-style.css">                                    
+      </head>
+
+      <body>
+        <div class="tm-main-content" id="top">
+            <div class="tm-top-bar-bg"></div>    
+            <div class="tm-top-bar" id="tm-top-bar">
+                <div class="container">
+                    <div class="row">
+                        <nav class="navbar navbar-expand-lg narbar-light">
+                            <a class="navbar-brand mr-auto" href="#">
+                                <img src="img/my logo.png" alt="Site logo">
+                                Tourism Guide
+                            </a>
+                            <button type="button" id="nav-toggle" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div id="mainNav" class="collapse navbar-collapse tm-bg-white">
+                                <ul class="navbar-nav ml-auto">
+                                  <li class="nav-item">                                  
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#tm-section-2">Top Destinations</a>
+                                </li>                               
+                            </ul>
+                        </div>                            
+                    </nav>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </div> <!-- .tm-top-bar -->
+        <div class="tm-container-outer" id="tm-section-2">
+                <section class="tm-slideshow-section">
+                    <div class="tm-slideshow">
+                        <img src="img/1.jpg" alt="Image">
+                        <img src="img/2.jpg" alt="Image">
+                        <img src="img/3.jpg" alt="Image">    
+                    </div>
+                    <div class="tm-slideshow-description tm-bg-primary">
+                        <h2 class="">Poctoy White Beach is the most visited places in Marinduque, Philippines</h2>
+                        <p>Poctoy White Beach, located in the coastal town of Torrijos in Marinduque, is one of the popular beaches in the Philippines. The beach features a kilometer stretch of fine white 
+                            sand and pristine blue waters that is perfect for swimming and snorkeling. The place also gives 
+                            a scenic view of Mount Malindig, the highest point in the province that is famous among beginner 
+                            mountain climbers. Environment fee for:
+                         Adult is 50 |
+                         Children is 30 |
+                         Senior citizen is 40 |
+                         Pavilion cottage is 300 |
+                         Use of view deck alone is 500 |
+                         Use of view deck for conference 1500 |
+                         Air conditioned cottage/room is 1500 |
+                          📞: 0921 594 2196 | 
+                        4.4⭐⭐⭐⭐ reviews;Loved the white beach! Affordable cottage and friendly staff! | 
+                           No need to go to Boracay, we have here at Marinduque.It was raining the day we arrived here, but still the place was good, the beach area was clean and there were some lifeguards hanging around, The entrance fee was 50 pesos per person, 
+                           and they had a shower and toilet area that was also clean. We will be back here on a good day!</p>
+                        <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</a>
+                    </div>
+                </section>
+                
+                 <section class="clearfix tm-slideshow-section tm-slideshow-section-reverse">
+
+                    <div class="tm-right tm-slideshow tm-slideshow-highlight">
+                        <img src="img/4.jpg" alt="Image">
+                        <img src="img/5.jpg" alt="Image">
+                        <img src="img/6.jpg" alt="Image">
+                    </div> 
+
+                    <div class="tm-slideshow-description tm-slideshow-description-left tm-bg-highlight">
+                        <h2 class="">Moriones Festival is the most popular event in Marinduque, Philippines</h2>
+                        <p>The Moriones is a lenten festival held annually on Holy Week on the island of Marinduque, Philippines. The "Moriones" are men and women in costumes and masks replicating the garb of biblical Imperial Roman soldiers as interpreted by locals. The Moriones tradition has inspired the creation
+                             of other festivals in the Philippines where cultural practices is turned into street festivals.
+                             The Moriones is a lenten festival held annually on Holy Week on the island of Marinduque, Philippines.📆	April or March</p>
+                        <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-highlight">Continue Reading</a>
+                    </div>                        
+
+                </section>
+                <section class="tm-slideshow-section">
+                    <div class="tm-slideshow">
+                        <img src="img/7.jpg" alt="Image">
+                        <img src="img/8.jpg" alt="Image">
+                        <img src="img/9.jpg" alt="Image">
+                    </div>
+                    <div class="tm-slideshow-description tm-bg-primary">
+                        <h2 class="">Kusina sa Plaza is the most famous restaurat in Marinduque, Philippines</h2>
+                        <p>Kusina Sa Plaza is a Event management company located at Mercader St, Boac, Marinduque  PH. This is Kusina sa Plaza, the home for traditional/authentic Marinduque cuisine. They also have the
+                             Café on the corner and a fine dining restaurant on the second floor called Casa De Don Emilio.Service options: Dine-in · Takeout
+                             📍: Mercader St, Boac, Marinduque |
+                             Hours: Everyday 7 AM–6 PM |
+                             📞: (042) 332 1699; |
+                             3.9⭐⭐⭐⭐ reviews | Quaint dining place. Food is good, self service.This is my goto place in Boac,Marinduque,
+                              Philippines. The staff is attentive and friendly.Good food. Good price. It's not perfect but 
+                              it's one of your best choice in Marinduque.</p>
+                        <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</a>
+                    </div>
+                </section>
+            </div>        
+        </section>
+        <section class="tm-slideshow-section">
+            <div class="tm-slideshow">
+                <img src="img/10.jpg" alt="Image">
+                <img src="img/11.jpg" alt="Image">
+                <img src="img/12.jpg" alt="Image">
+            </div>
+            <div class="tm-slideshow-description tm-bg-primary">
+                <h2 class="">Balar Hotel and Spa is the most popular accommodation in Marinduque, Philippines</h2>
+                <p>Balar Hotel and Spa has a garden, terrace, a restaurant and bar in Boac. The accommodation features a 24-hour front desk, airport transfers, room service and free WiFi.
+                    At the hotel, rooms include a desk, a flat-screen TV, a private bathroom, bed linen and towels. Every room includes a safety deposit box, while certain rooms have a balcony and others also boast garden views. The rooms will provide guests with a wardrobe and a kettle.
+                   A buffet, à la carte or continental breakfast can be enjoyed at the property.What are the check-in and check-out times at Balar Hotel and Spa?
+                    The check-in time is from 14:00 to 16:00 and the check-out time is before 12:00. | 📍 CR9C+JVG, Brgy. Balaring Building, Boac, 4900 |
+                    PRICE RANGE:
+                   ₱2,801 - ₱3,529 (Based on Average Rates for a Standard Room)
+                   📞: 0917 882 2527 |
+                   Reviews 4.5⭐⭐⭐⭐Very good |Reviews: The staff are so sweet and accommodating. They even prepared food for us on our island hopping trip because we set out so early that we won't have our breakfast at the hotel. |
+                   Rooms: 5/5  |  Service: 5/5  |  Location: 5/5 |
+                   Hotel details |
+                   Free Wi-Fi | Free parking  |  Laundry service  |  Beach access  |  Restaurant | Airport shuttle
+                  Hotel highlights
+                   Great value</p>
+                <a href="#" class="text-uppercase tm-btn tm-btn-white tm-btn-white-primary">Continue Reading</a>
+            </div>
+        </section>
+    </div>     
